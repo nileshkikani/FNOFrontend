bdeb043f812e20999c6a6be33e04a3af233f8825
rutvik jamvecha <rutvik.jamvecha@logisticinfotech.co.in>
2024-06-05 22:06:33 +0530

    redux updated

--- store/index.js
+++ store/index.js
@@ -3,46 +3,24 @@ import { useDispatch, TypedUseSelectorHook, useSelector } from 'react-redux';
 import { persistReducer } from 'redux-persist';
 import { authReducer } from '@/store/authSlice';
 import { userReducer } from '@/store/userSlice';
-import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
+import storage from "redux-persist/lib/storage"
 
-const createNoopStorage = () => {
-  return {
-    getItem() {
-      return Promise.resolve(null);
-    },
-    setItem(_key, value) {
-      return Promise.resolve(value);
-    },
-    removeItem() {
-      return Promise.resolve();
-    }
-  };
-};
 
-const storage = typeof window !== 'undefined' ? createWebStorage('local') : createNoopStorage();
-
-const authPersistConfig = {
-  key: 'auth',
-  storage: storage,
-  whitelist: ['authState', 'isUser', 'isCookie', 'rememberMe']
-};
-
-const userPersistConfig = {
-  key: 'user',
-  storage: storage,
-  whitelist: ['userEmail']
-};
-
-const persistedReducer = persistReducer(authPersistConfig, authReducer);
-const persistedUserReducer = persistReducer(userPersistConfig, userReducer);
+const persistConfig = {
+  key: "root",
+  storage,
+}
 
 const rootReducer = combineReducers({
-  auth: persistedReducer,
-  user: persistedUserReducer
+  auth: authReducer,
+  user: userReducer
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false })
 });
 
